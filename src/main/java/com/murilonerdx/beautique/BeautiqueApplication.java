2902aae5346d3cd7dceb33d30fa1c35b494acff4
Murilo P. S <mu-silva@outlook.com>
2025-07-21 08:40:12 -0300

    feat: projeto inicial

---
+++ src/main/java/com/murilonerdx/beautique/BeautiqueApplication.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.beautique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilonerdx/beautique/BeautiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.beautique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
