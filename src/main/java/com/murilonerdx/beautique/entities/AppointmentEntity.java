43ed32b2a1781d7bcf10aef3f258858e48725e83
Murilo P. S <mu-silva@outlook.com>
2025-07-21 09:29:27 -0300

    feat: adicionando entidades

--- src/main/java/com/murilonerdx/beautique/entities/AppointmentEntity.java
+++ src/main/java/com/murilonerdx/beautique/entities/AppointmentEntity.java
@@ -1,4 +1,35 @@
 package com.murilonerdx.beautique.entities;
 
-public class AppointmentEntity {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "appointments")
+public class AppointmentEntity extends BaseEntity{
+
+	@Column(nullable = false, updatable = true)
+	private LocalDateTime dateTime;
+
+	@Column(nullable = false)
+	private Boolean appointmentsOpen;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "customer_id", nullable = true)
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private CustomerEntity customer;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "beauty_procedure_id", nullable = true)
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private BeautyProceduresEntity beautyProcedure;
+
 }
--- src/main/java/com/murilonerdx/beautique/entities/BeautyProceduresEntity.java
+++ src/main/java/com/murilonerdx/beautique/entities/BeautyProceduresEntity.java
@@ -1,4 +1,33 @@
 package com.murilonerdx.beautique.entities;
 
-public class BeautyProceduresEntity {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "beauty_procedures")
+public class BeautyProceduresEntity extends BaseEntity {
+
+	@Column(nullable = false, length = 100)
+	private String name;
+
+	@Column(length = 500)
+	private String description;
+
+	@Column(nullable = false)
+	private BigDecimal price;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "beautyProcedure", cascade = CascadeType.ALL, orphanRemoval = true)
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	private List<AppointmentEntity> appointments;
 }
